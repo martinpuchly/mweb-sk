ef20ac2b851399a90474db149e274c553cf73a77
martinpuchly <martin.puchly@gmail.com>
2022-08-17 21:48:50 +0200

    zakladne nastavenie

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row" style="border-bottom-style: solid; border-color: rgba(34,34,70,0.5);;">
+            <div class="col-8" style="font-style: italic; font-size: 3em; margin-left: 1em;">
+                mWeb.sk
+            </div>
+        </div>
+        <div class="row">
+            <SidePanel class="col-3 px-2 pt-4"></SidePanel >
+            <main class="col-9 pt-4">
+                <slot />
+            </main>
+        </div>
+
+    </div>
+</template>
+
+
+<script>
+    import SidePanel from './SidePanel.vue';
+    export default{
+        components:{
+            SidePanel
+        }
+
+    }
+
+
+</script>
---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+import AppLayout from '@/Layouts/AppLayout.vue';
+import BreezeInputError from '@/Components/InputError.vue';
+defineProps({
+    canResetPassword: Boolean,
+    status: String,
+});
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => form.reset('password'),
+    });
+};
+</script>
+
+<template>
+    <AppLayout>
+        <Head title="prihlásiť sa" />
+        <h1>Prihlásiť</h1>
+        <form @submit.prevent="submit">
+        <div class="col-5">
+            <div class="form-group">
+                    <label for="email" class="form-label">Emailová adresa:</label>
+                    <input type="text" name="email" id="email" class="form-control" v-model="form.email">
+                    <BreezeInputError class="mt-2 text-danger" :message="form.errors.email" />
+
+            </div>
+            <div class="form-group mt-3">
+                    <label for="password" class="form-label">Heslo:</label>
+                    <input type="text" name="password" id="password" class="form-control" v-model="form.password">
+                    <BreezeInputError class="mt-2 text-danger" :message="form.errors.password" />
+
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="remember" id="remember" class="form-check-input">
+                <label for="remember" class="form-check-label ml-3"> zapamätať si prihlásenie</label>
+            </div>
+            <div class="form-group text-center">
+                <button class="btn btn-primary mt-4">Prihlásiť </button>
+            </div>
+        </div>
+        </form>
+    </AppLayout>
+</template>
