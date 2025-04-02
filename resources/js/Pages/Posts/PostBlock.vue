206add78cd5d36d5323143f74c905d168259a7d1
martinpuchly <martin.puchly@gmail.com>
2025-04-02 21:28:23 +0200

    uprava tags

--- resources/js/Pages/Posts/PostBlock.vue
+++ resources/js/Pages/Posts/PostBlock.vue
@@ -5,13 +5,13 @@
                 <h3>{{ post.title }}</h3>
             </header>
             <main v-html="post.intro" style="overflow-wrap: break-word;" class="quill-bu"></main>
-            <div class="hstack gap-3" v-if="post.tags">
+            <div class="flex" v-if="post.tags">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-tags mx-1" viewBox="0 0 16 16">
                     <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z"/>
                     <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z"/>
                 </svg>
                 <span v-for="(tag, index) in post.tags_array" v-key="index">
-                    <Link :href="route('posts', {tag: tag})">#{{ tag }}</Link>, 
+                    <Link :href="route('posts', {tag: tag})">#{{ tag }}</Link>,	&nbsp;
                 </span>
             </div>
             <footer class="fst-italic border-bottom text-end w-100">
--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -10,13 +10,13 @@
         </main>
         <br clear="all">
         <footer class="w-100 border-top">
-            <section v-if="post.tags">
+            <section v-if="post.tags" class="flex">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-tags mx-1" viewBox="0 0 16 16">
-                <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z"/>
-                <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z"/>
+                     <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z"/>
+                    <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z"/>
                 </svg>
                 <span v-for="(tag, index) in post.tags_array" v-key="index">
-                    <Link :href="route('posts', {tag: tag})">#{{ tag }}</Link>, 
+                    <Link :href="route('posts', {tag: tag})">#{{ tag }}</Link>,	&nbsp; 
                 </span>
             </section>
             <section class="text-end italic">
